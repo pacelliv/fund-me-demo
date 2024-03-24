e611101481d52cdd2ad7f170532ef69313b1812f
pacelliv <flores.eugenio03@gmail.com>
2024-03-23 19:37:54 -0500

    footer links

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,11 +5,17 @@ const Footer = () => {
         <footer className="bg-[#0c0b0e] py-12">
             <div className="mx-auto flex w-11/12 max-w-[1110px] flex-col items-center">
                 <div>
-                    <a href="/" target="_blank" className=" text-[#7f63ef] hover:text-[#6751c2]">
+                    <a
+                        href="https://twitter.com/pacelliv3"
+                        className=" text-[#7f63ef] hover:text-[#6751c2]"
+                    >
                         Twitter
                     </a>
                     <span className="ml-3 mr-3">|</span>
-                    <a href="/" target="_blank" className="text-[#7f63ef] hover:text-[#6751c2]">
+                    <a
+                        href="https://github.com/pacelliv/fund-me-demo"
+                        className="text-[#7f63ef] hover:text-[#6751c2]"
+                    >
                         GitHub
                     </a>
                 </div>
