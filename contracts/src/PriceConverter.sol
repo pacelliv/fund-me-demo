10c401a61789e352b8f1da89e8fdc9d1180fdcaa
pacelliv <flores.eugenio03@gmail.com>
2024-03-25 11:16:46 -0500

    changes

--- contracts/src/PriceConverter.sol
+++ contracts/src/PriceConverter.sol
@@ -7,12 +7,12 @@ library PriceConverter {
     function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {
         (, int256 answer,,,) = priceFeed.latestRoundData();
         // ETH/USD rate in 18 digit
-        return uint256(answer * 10000000000);
+        return uint256(answer * 1e10); // 1e10 = 10000000000
     }
 
     function getConversionRate(uint256 ethAmount, AggregatorV3Interface priceFeed) internal view returns (uint256) {
         uint256 ethPrice = getPrice(priceFeed);
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18; // 1e18 = 1000000000000000000
         // the actual ETH/USD conversation rate, after adjusting the extra 0s.
         return ethAmountInUsd;
     }
