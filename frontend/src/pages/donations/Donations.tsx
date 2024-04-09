5fb80969dd8c59d130fc9f37ef008bafce0b7632
pacelliv <flores.eugenio03@gmail.com>
2024-04-09 17:13:14 -0500

    switched main tags for section tags

--- frontend/src/pages/donations/Donations.tsx
+++ frontend/src/pages/donations/Donations.tsx
@@ -32,14 +32,14 @@ const Donations = () => {
                 title="FundMe | Donations"
                 description="Donations page. On this pages the users can see all the history donations to the contract. The data is displayed in a table."
             />
-            <main className="mx-auto w-11/12 max-w-[1000px] pb-16">
+            <section className="mx-auto w-11/12 max-w-[1000px] pb-16">
                 <h1 className="mb-8 w-4/5 text-4xl font-bold leading-snug text-white min-[750px]:w-72 min-[960px]:w-96 lg:text-5xl">
                     All Donations
                 </h1>
                 <Suspense fallback={<SkeletonCard />}>
                     <Await resolve={data.eventsData}>{renderTable}</Await>
                 </Suspense>
-            </main>
+            </section>
         </>
     );
 };
