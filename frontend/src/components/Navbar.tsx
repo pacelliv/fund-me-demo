23ead80dfa2c677f0ad93439eb9bedc027e06875
pacelliv <flores.eugenio03@gmail.com>
2024-04-09 13:23:00 -0500

    wrapped navbar in header tag

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,15 +3,17 @@ import ConnectButton from "./ConnectButton";
 
 const Navbar = () => {
     return (
-        <nav className="mx-auto mb-10 flex w-11/12 max-w-[1110px] items-center py-5 pl-2 text-white">
-            <Link to="/" className="text-lg font-bold max-[460px]:hidden">
-                FundMe
-            </Link>
-            <Link to="donations" className="ml-auto mr-6 max-[460px]:ml-0 max-[460px]:mr-auto">
-                Donations
-            </Link>
-            <ConnectButton />
-        </nav>
+        <header>
+            <nav className="mx-auto mb-10 flex w-11/12 max-w-[1110px] items-center py-5 pl-2 text-white">
+                <Link to="/" className="text-lg font-bold max-[460px]:hidden">
+                    FundMe
+                </Link>
+                <Link to="donations" className="ml-auto mr-6 max-[460px]:ml-0 max-[460px]:mr-auto">
+                    Donations
+                </Link>
+                <ConnectButton />
+            </nav>
+        </header>
     );
 };
 
